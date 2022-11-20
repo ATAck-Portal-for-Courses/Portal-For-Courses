79dbb3b54db88910ecf50fd628e063838b176e54
CodeineX <ayushramteke2201@gmail.com>
2022-11-20 21:29:01 +0530

    fixed position of footer

--- atack-portal/frontend/src/components/pages/AdminHome.js
+++ atack-portal/frontend/src/components/pages/AdminHome.js
@@ -1,12 +1,33 @@
+/* eslint-disable jsx-a11y/alt-text */
 import React from 'react'
-import { Link } from 'react-router-dom'
 import HomePage from './HomePage'
+
+function Card(props){
+    return(
+        <div className="card">
+            <div className="card_body">
+                <img className='card_img'
+                src={props.img} />
+                <h2 className="card_title">{props.card_title}</h2>
+                <p className="card_description">
+                    {props.card_desc}
+                </p>
+                <button className="card_btn">View Course</button>
+            </div>
+        </div>
+    )
+}
 
 const AdminHome= () => {
     return (
         <>
-            <div>
-                hemlo
+            <div className="header">Home</div>
+            <div className='wrapper'>
+                <Card
+                img="https://images.unsplash.com/photo-1633493702341-4d04841df53b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8cGh5c2ljc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=1000&q=60"
+                card_title="PH108"
+                card_desc="Physics course offered in the spring semester for UG freshies"
+                />
             </div>
             <HomePage/>
         </>
