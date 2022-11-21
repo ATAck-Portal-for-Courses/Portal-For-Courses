8e37f3682e450d76286c5695bdda3241722d3d05
CodeineX <ayushramteke2201@gmail.com>
2022-11-21 16:37:00 +0530

    changes made to config.js, set up backend server

--- atack-portal/backend/db/config.js
+++ atack-portal/backend/db/config.js
@@ -1,2 +1,2 @@
 const mongoose = require("mongoose");
-mongoose.connect("mongodb+srv://coolboy19:1234567890@atack-portal.02hznp7.mongodb.net/Atack-portal");
+mongoose.connect("mongodb+srv://ayush2201:ayush12345@atack-portal.02hznp7.mongodb.net/Atack-portal");
