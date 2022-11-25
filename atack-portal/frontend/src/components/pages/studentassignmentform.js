f780d564476e5444c79b1a5551537e19384a743b
aksh-at10 <akshatkumarg4@gmail.com>
2022-11-25 15:48:34 +0530

    Student Assignment page navigate

--- atack-portal/frontend/src/components/pages/studentassignmentform.js
+++ atack-portal/frontend/src/components/pages/studentassignmentform.js
@@ -1,8 +1,58 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useNavigate } from "react";
 import CourseNav from "../navbar/CourseNav";
+
+// const [assignment,setAssignment] = useState('');
+    // const [file, setFile]=useState('')
+
 
 const AssignmentPage = ()=>{
     
+
+    // const navigate = useNavigate();
+    
+    const collectData = async () => {
+
+        let courseCode = JSON.parse(localStorage.getItem("course")).courseCode
+        let studentid = JSON.parse(localStorage.getItem("student")).username
+        // console.warn(courseCode);
+    
+        let assignmentId = window.location.pathname;
+        assignmentId = assignmentId.substring(1);
+        assignmentId = assignmentId.split('/')[1];
+    
+        let Submission_formData = new FormData()
+        Submission_formData.append('courseCode', courseCode)
+        Submission_formData.append("username", studentid)
+        Submission_formData.append("assignmentId", assignmentId)
+        Submission_formData.append("file", file)
+        
+
+        console.log(typeof(file))
+        const fileString = JSON.stringify(file)
+        console.warn(fileString)
+        // let payload = {}
+        let result = await fetch('http://localhost:7000/addAssignment',{
+          method:"post",
+          // body:JSON.stringify({courseCode, assignmentName, description, startDate,dueDate, fileString}),
+          body: Submission_formData,
+          // headers:{
+          //   'Content-Type':'application/json'
+          
+          // }
+        })
+  
+        result = await result.json();
+  
+        console.log(result)
+  
+        if(!result) alert("Assignment already created");
+        else{
+          let id = JSON.parse(localStorage.getItem("course"))._id;
+        //   navigate(`/${id}`)
+        }
+  
+    }
+
     let assignmentId = window.location.pathname;
     assignmentId = assignmentId.substring(1);
     assignmentId = assignmentId.split('/')[1];
@@ -51,10 +101,13 @@ const AssignmentPage = ()=>{
 
 
             <div class="form-group mt-3">
-            <label class="mr-2">Upload Assignment:</label><br/>
+            <label class="mr-2">Submit Assignment:</label><br/>
             <input type="file" name="file" multiple onChange={(e)=>setFile(e.target.files[0])} />
             </div>
-            <button type="button" class="btn btn-primary">Submit</button>
+
+            <p>Grades: </p>
+            <p>Feedback: </p>
+            <button type="button" class="btn btn-primary" >Submit</button>
           </form>
           
             
